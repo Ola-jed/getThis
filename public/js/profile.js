37c9cae36ecf2c99a02ff3cc9a82e1cfc9b6173e
olabijed@gmail.com <olabijed@gmail.com>
2021-05-30 18:40:03 +0100

    Script profile for the form upated

--- public/js/profile.js
+++ public/js/profile.js
@@ -4,6 +4,9 @@ const updateBtn = document.querySelector(".update");
 // Is the update form visible or not ?
 let isVisible = false;
 
+/**
+ * Show and hide the update account form on click
+ */
 updateBtn.onclick = function () {
     isVisible = !isVisible;
     profileUpdateForm.style.display = isVisible ? 'block' : 'none';
