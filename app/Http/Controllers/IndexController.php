14d5335dc603443900fe3c00b3e87f1cbdfacc75
olabijed@gmail.com <olabijed@gmail.com>
2021-06-22 19:16:56 +0100

    Index controller made invokable

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -8,7 +8,6 @@ use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\RedirectResponse;
-use function session;
 
 /**
  * Class IndexController
@@ -21,7 +20,7 @@ class IndexController extends Controller
      * Load index data and call the view
      * @return Application|Factory|View|RedirectResponse
      */
-    public function index(): Application|Factory|View|RedirectResponse
+    public function __invoke(): Application|Factory|View|RedirectResponse
     {
         return view('index')->with([
             'articles' => Article::getLatest(5),
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,6 @@
 use App\Http\Controllers\IndexController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/',[IndexController::class,'index'])
+Route::get('/',IndexController::class)
     ->name('index')
     ->middleware('session');
