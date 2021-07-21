7a6b7e2243640c4294e88058edd8f09d73af0167
olabijed@gmail.com <olabijed@gmail.com>
2021-07-21 11:36:21 +0100

    Markdown mail update

--- resources/views/emails/report.blade.php
+++ resources/views/emails/report.blade.php
@@ -1,13 +1,17 @@
 @component('mail::message')
-# Report of a new User
+# User reported
 
 <p>A user have been reported</p>
-<p>Here are the information : </p>
 
+<p>Here are the user information : </p>
 <ul>
     <li>Id : {{ $user->id }}</li>
     <li>Name : {{ $user->name }}</li>
     <li>Email : {{ $user->email }}</li>
 </ul>
 
+<p>Report cause :</p>
+@component('mail::panel')
+    {!! $message !!}
+@endcomponent
 @endcomponent
