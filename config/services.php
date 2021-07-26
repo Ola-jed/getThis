02205b7a2192952714bfd018ceb3cb177ba2a479
olabijed@gmail.com <olabijed@gmail.com>
2021-07-26 14:05:25 +0100

    Socialite redirect url

--- config/services.php
+++ config/services.php
@@ -33,7 +33,7 @@ return [
     'google' => [
         'client_id'     => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-        'redirect'      => env('APP_URL') . '/login/google/callback',
+        'redirect'      => env('APP_URL') . 'login/google/callback',
     ],
 
     'github' => [
